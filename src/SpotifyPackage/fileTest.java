3f1ae95005597ce64343e095a5c28e9b9003327b
Bishobos <mps.kajkage@gmail.com>
2025-09-25 14:59:03 +0200

    File reader and file writer works.

--- src/SpotifyPackage/fileTest.java
+++
@@ -1,9 +0,0 @@
-package SpotifyPackage;
-
-public class fileTest {
-    public static void main(String[] args){
-        fileReader r = new fileReader("src/SpotifyPackage/songlist.csv");
-        Song song = new Song(r.reader().getFirst().getFirst(), r.reader().getFirst().getFirst());
-        System.out.println(song);
-    }
-}
